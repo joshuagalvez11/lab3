141497226bd61457310305a5c674fcf35593caee
Joshua Galvez <joshua.galvez.559@my.csun.edu>
2023-04-29 16:18:04 -0700

    added all files

--- index.js
+++
@@ -1,6 +0,0 @@
-const emailRegex = 
- new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
-
- const isValidEmail = emailRegex.test("email_address@domain.com");
-
-console.log(isValidEmail) //true
